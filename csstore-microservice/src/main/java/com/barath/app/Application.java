abc9bc2cdebe198cc25005b68093e43e0e5c9186
Barath Arivazhagan <barathmacdec91@gmail.com>
2017-08-22 23:13:09 +0530

    adding eureka server and service discovery

---
+++ csstore-microservice/src/main/java/com/barath/app/Application.java
@@ -0,0 +1,22 @@
+package com.barath.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+	@GetMapping("/")
+	public String home(){
+		return "WELCOME TO COSTCO STORE ";
+	}
+}
