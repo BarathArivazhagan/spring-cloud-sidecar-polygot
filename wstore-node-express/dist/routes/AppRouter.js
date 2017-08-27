491df7d7dfcaa61f2296a6a21d62cdc39c9fd6b6
Barath Arivazhagan <barathmacdec91@gmail.com>
2017-08-27 20:32:17 +0530

    adjusting the properties

--- wstore-node-express/dist/routes/AppRouter.js
+++ wstore-node-express/dist/routes/AppRouter.js
@@ -8,6 +8,10 @@ class AppRouter {
         router.get('/', (req, res) => {
             res.send('WELCOME TO WALMART NODE EXPRESS APPLICATION !');
         });
+        router.get('/health', (req, res) => {
+            console.log("Health json is called ");
+            res.json({ "status": "UP" });
+        });
         express.use('/', router);
     }
 }
